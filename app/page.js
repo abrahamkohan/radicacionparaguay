e6e2fd8cf6142d785a542f53f30beb9ff65c7aab
abrahamkohan <92029796+abrahamkohan@users.noreply.github.com>
2026-03-24 18:38:22 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -16,8 +16,8 @@ export default function RadicacionPYLanding() {
       pasaporte: false,
       nacimiento: false,
       antecedentes: false,
-      matrimonio: false
-      foto:false
+      matrimonio: false,
+      foto:false,
     },
     archivos: {
       pasaporte: null,
