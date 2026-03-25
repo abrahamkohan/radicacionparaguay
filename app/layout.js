7d1512e80c74765b375eeefc179e7d44dd762708
abrahamkohan <92029796+abrahamkohan@users.noreply.github.com>
2026-03-24 21:35:20 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,29 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Radicación Paraguay - Ley N° 6984/22',
-  description: 'Evaluación de radicación con seguridad jurídica',
+  title: 'Radicación Paraguaya - Ley Nº 6984/22',
+  description: 'Somos líderes en la gestión de todo tipo de trámites vinculados con la residencia en Paraguay, con testimonios de miles de personas de todo el mundo.',
+  openGraph: {
+    title: 'Radicación Paraguaya - Ley Nº 6984/22',
+    description: 'Evaluación de radicación con seguridad jurídica.',
+    url: 'https://abrahamkohan.com.py',
+    siteName: 'Radicación en Paraguay',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'es_PY',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Radicación Paraguay',
+    description: 'Evaluación migratoria en Paraguay',
+    images: ['/og-image.jpg'],
+  },
 }
 
 export const viewport = 'width=device-width, initial-scale=1'
